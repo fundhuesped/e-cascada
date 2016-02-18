444154b99cabeeedab0155e8ffeacb8b89b2ca87
Nicolas Lelio Gonzalez <nicolas.lelio.gonzalez@gmail.com>
2016-02-18 02:01:31 -0300

    Solucionado if

--- huesped_backend/critical_settings.py
+++ huesped_backend/critical_settings.py
@@ -8,7 +8,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if(os.environ['SECRET_KEY'])
+if os.environ['SECRET_KEY']:
 	SECRET_KEY = os.environ['SECRET_KEY']
 
 # Database
