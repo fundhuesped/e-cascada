b82562e2b1f6cc4044021775aea77133d5a912e8
Ruben Ghio <rubenghio@gmail.com>
2016-02-20 15:25:42 -0300

    Agregado de entidad Period #HUES-44

--- procedures/urls.py
+++ procedures/urls.py
@@ -4,4 +4,5 @@ from procedures import views
 app_name = "procedures"
 urlpatterns = [
     url(r'^period/$', views.PeriodList.as_view(), name='Period-list'),
+    url(r'^period/(?P<pk>[0-9]+)/$', views.PeriodDetails.as_view(),name='Period-detail'),
 ]
