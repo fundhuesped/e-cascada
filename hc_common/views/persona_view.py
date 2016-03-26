9181f176777e2aaa7c82d4c44bf1883b617a285c
Ruben Ghio <rubenghio@gmail.com>
2016-03-26 14:41:42 -0300

    #HUES-37, #HUES-38, #HUES-39, #HUES-40, #HUES-41

--- hc_common/views/persona_view.py
+++ hc_common/views/persona_view.py
@@ -23,10 +23,13 @@ class PersonaList(generics.ListCreateAPIView):
         queryset = Persona.objects.all()
         firstName = self.request.query_params.get('firstName')
         fatherSurename = self.request.query_params.get('fatherSurename')
-        if (firstName is not None and len(firstName)>3):
+        status = self.request.query_params.get('status')
+        if firstName is not None and len(firstName)>3:
             queryset = queryset.filter(firstName__startswith=firstName)
-        if (fatherSurename is not None and len(fatherSurename)>3):
+        if fatherSurename is not None and len(fatherSurename)>3:
             queryset = queryset.filter(fatherSurename__startswith=fatherSurename)
+        if status is not None:
+            queryset = queryset.filter(status=status)
         return queryset
 
 class PersonaDetails(generics.RetrieveUpdateDestroyAPIView):
