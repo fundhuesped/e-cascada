9204e5d77f28921da74f472443e2eb4bccecd6d6
Nicolas Lelio Gonzalez <nicolas.lelio.gonzalez@gmail.com>
2016-04-21 00:24:41 -0300

    Added paciente filter to turno, modified taken filter in turno, added order in turno

--- hc_practicas/views/turno_view.py
+++ hc_practicas/views/turno_view.py
@@ -15,9 +15,12 @@ class TurnoList(generics.ListCreateAPIView):
     paginate_by = 20
 
     def get_queryset(self):
+        queryset = Turno.objects.all()
         taken = self.request.query_params.get('taken')
-        value = self.str2bool(taken)    
-        queryset = Turno.objects.filter(taken=value)
+        
+        if taken is not None:
+            value = self.str2bool(taken)    
+            queryset = queryset.filter(taken=value)
         #day_from = self.request.query_params.get('from')
         #if day_from is None:
         #    day_from = datetime.now()
@@ -28,12 +31,32 @@ class TurnoList(generics.ListCreateAPIView):
         day = self.request.query_params.get('day')
         if day is not None:
             queryset = queryset.filter(day=day)
+
         prestacion = self.request.query_params.get('prestacion')
         if prestacion is not None:
             queryset = queryset.filter(prestacion=prestacion)
         profesional = self.request.query_params.get('profesional')
         if profesional is not None:
             queryset = queryset.filter(profesional=profesional)
+
+        paciente = self.request.query_params.get('paciente')
+        if paciente is not None:
+            queryset = queryset.filter(paciente = paciente)
+
+        #Order  
+        order_field = self.request.query_params.get('order_field')
+        order_by = self.request.query_params.get('order_by')
+        if (order_field is not None) and (order_by is not None):
+            print order_field
+            print order_by
+            if order_by is 'asc':
+                print 'es asc'
+                queryset = queryset.order_by(order_field)
+            else:
+                print 'es desc'
+                if order_by is 'desc':
+                    queryset = queryset.order_by('-'+order_field)
+
 
         return queryset
 
