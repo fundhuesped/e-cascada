ea7030050ad132f0056d71968f0eae94ac227eb8
Andres Calabrese <acalabrese@gmail.com>
2016-06-13 08:18:33 -0300

    Unifica config de branches master / develop

--- huesped_backend/critical_settings.py
+++ huesped_backend/critical_settings.py
@@ -17,23 +17,29 @@ SECRET_KEY = os.getenv('SECRET_KEY','SECRET_KEY_HARDCODED')
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'd8g8l9ial8njr9',
-#        'USER': 'zdrbseqquojvqi',
-#        'PASSWORD': '0RbvCFbj7icCZ16SNllAa7m7jL',
-#        'HOST': 'ec2-54-83-0-187.compute-1.amazonaws.com',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+DB_NAME = os.getenv('DB_NAME','DB_NAME')
+DB_USER = os.getenv('DB_USER','DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD','DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST','DB_HOST')
+DB_PORT = os.getenv('DB_PORT','DB_PORT')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
 
 #Configuracion CORS
 CORS_ORIGIN_ALLOW_ALL = True
