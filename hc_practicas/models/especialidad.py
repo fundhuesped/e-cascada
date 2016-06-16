f68e7742736a5bf7448d50cbe4183fc96de62c9d
Nicolas Lelio Gonzalez <nicolas.lelio.gonzalez@gmail.com>
2016-06-16 00:25:27 -0300

    #HUES-246 Description filed no longer required for especialidad

--- hc_practicas/models/especialidad.py
+++ hc_practicas/models/especialidad.py
@@ -7,5 +7,5 @@ from hc_common.models import ActiveModel
 
 class Especialidad(ActiveModel):
     name = models.CharField(max_length=70, null=False)
-    description = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=150, null=True, blank=True)
     status = models.CharField(max_length=8, choices=ActiveModel.STATUS_CHOICES, default=ActiveModel.STATUS_ACTIVE)
