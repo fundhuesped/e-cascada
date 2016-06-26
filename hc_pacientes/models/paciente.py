31f149923de38cc542964337fcfa81901b3ff1da
Nicolas Lelio Gonzalez <nicolas.lelio.gonzalez@gmail.com>
2016-06-26 20:59:48 -0300

    #HUES-254 added consent field with options to paciente

--- hc_pacientes/models/paciente.py
+++ hc_pacientes/models/paciente.py
@@ -8,5 +8,16 @@ class Paciente(Persona):
     """
     Clase que representa la información mínima necesaria para gestionar un Paciente
     """
+    CONSENT_YES = 'Yes'
+    CONSENT_NO = 'No'
+    CONSENT_NA = 'Not asked'
+
+    CONSENT_CHOICES = (
+        (CONSENT_YES, 'Si'),
+        (CONSENT_NO, 'No'),
+        (CONSENT_NA, 'No preguntado')
+    )
+
     idpaciente = models.CharField(max_length=20, null=True)
     prospect = models.BooleanField(default=False)
+    consent = models.CharField(max_length=14, choices=CONSENT_CHOICES, default=CONSENT_NA)
